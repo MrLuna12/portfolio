444032ab889e34a5f44bb555b550778d4485b14e
Michael Luna <michaelluna244@gmail.com>
2025-10-14 22:43:15 -0700

    Create Home page with navigation and placeholder text

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+import Navigation from '../components/Navigation'
+
+function Home() {
+  return (
+    <>
+      <Navigation />
+      <div className="min-h-screen pt-20">
+        <h1 className="text-4xl font-bold text-center pt-24">Home Page</h1>
+        <p className="text-center text-gray-600 mt-4">
+          This will contain: Hero, About, Experience, Projects, Contact sections
+        </p>
+        
+        <div className="text-center mt-8">
+          <Link to="/projects" className="text-blue-500 underline">
+            Go to Projects Page
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
