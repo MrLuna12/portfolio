fa35c612dca72cdb22dc62e369f86d20d8cf35ae
Michael Luna <michaelluna244@gmail.com>
2025-10-15 22:21:51 -0700

    Refactor Home component to include HeroSection and remove placeholder content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,16 @@
 import { Link } from 'react-router-dom'
 import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
+import HeroSection from '../components/sections/HeroSection'
 
 function Home() {
-  return (
-    <>
-      <Navigation />
-      <div className="min-h-screen pt-20">
-        <h1 className="text-4xl font-bold text-center pt-24">Home Page</h1>
-        <p className="text-center text-gray-600 mt-4">
-          This will contain: Hero, About, Experience, Projects, Contact sections
-        </p>
-        
-        <div className="text-center mt-8">
-          <Link to="/projects" className="text-blue-500 underline">
-            Go to Projects Page
-          </Link>
-        </div>
-      </div>
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Navigation />
+            <HeroSection />
+            <Footer />
+        </>
+    )
 }
 
 export default Home
